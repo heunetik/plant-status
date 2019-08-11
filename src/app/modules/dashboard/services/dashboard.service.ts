60c7166a1bfd3a2cb8585e87341ae0209018fd75
heunetik <heunetik@gmail.com>
2019-08-11 18:51:28 +0300

    Linted project files

--- src/app/modules/dashboard/services/dashboard.service.ts
+++ src/app/modules/dashboard/services/dashboard.service.ts
@@ -6,6 +6,7 @@ import { map, catchError } from 'rxjs/operators';
 @Injectable()
 export class DashboardService {
     // Temporary gist URL
+    // tslint:disable-next-line:max-line-length
     private url = 'https://gist.githubusercontent.com/heunetik/0f5556ad8e9f522a2a421f1dd1a805c1/raw/7b23543498680c381090210decedbf5d4b578f46/BMEMock.json';
 
     constructor(private http: HttpClient) { }
